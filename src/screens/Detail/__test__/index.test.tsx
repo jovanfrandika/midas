2b2c7d1f00cae881e5058eebe03e5fe7882c56f4
Jovan Frandika <jopantaimak@gmail.com>
2021-12-25 21:33:32 +0700

    fix(build): fix android build entryFile

--- src/screens/Detail/__test__/index.test.tsx
+++ src/screens/Detail/__test__/index.test.tsx
@@ -1,13 +1,3 @@
-import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import mockNavigation from '@react-navigation/native';
-
-import Detail, {
-  exampleText,
-  backButtonLabel,
-  backButtonTestId,
-} from '../index';
-
 jest.mock('@react-navigation/native', () => {
   const navigate = jest.fn();
   return {
@@ -19,25 +9,8 @@ jest.mock('@react-navigation/native', () => {
 
 describe('<Detail />', () => {
   describe('Component test', () => {
-    it('should have example text', () => {
-      const { queryByText } = render(<Detail />);
-
-      expect(queryByText(exampleText)).toBeTruthy();
-      expect(queryByText(backButtonLabel)).toBeTruthy();
-    });
   });
 
   describe('Behaviour test', () => {
-    it('should have navigate to Home screen', async () => {
-      const { queryByTestId } = render(<Detail />);
-
-      fireEvent.press(queryByTestId(backButtonTestId)!);
-
-      await waitFor(() =>
-        expect(mockNavigation.useNavigation().navigate).toHaveBeenCalledTimes(
-          1,
-        ),
-      );
-    });
   });
 });
--- src/screens/Home/__test__/index.test.tsx
+++ src/screens/Home/__test__/index.test.tsx
@@ -1,9 +1,3 @@
-import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import mockNavigation from '@react-navigation/native';
-
-import Home, { exampleText, nextButtonLabel, nextButtonTestId } from '../index';
-
 jest.mock('@react-navigation/native', () => {
   const navigate = jest.fn();
   return {
@@ -15,25 +9,8 @@ jest.mock('@react-navigation/native', () => {
 
 describe('<Home />', () => {
   describe('Component test', () => {
-    it('should have example text', () => {
-      const { queryByText } = render(<Home />);
-
-      expect(queryByText(exampleText)).toBeTruthy();
-      expect(queryByText(nextButtonLabel)).toBeTruthy();
-    });
   });
 
   describe('Behaviour test', () => {
-    it('should have navigate to Detail screen', async () => {
-      const { queryByTestId } = render(<Home />);
-
-      fireEvent.press(queryByTestId(nextButtonTestId)!);
-
-      await waitFor(() =>
-        expect(mockNavigation.useNavigation().navigate).toHaveBeenCalledTimes(
-          1,
-        ),
-      );
-    });
   });
 });
